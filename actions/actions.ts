18b621b7b6375e9bc2436d372a9672f4bae59f3c
Suhel <suhel7822@gmail.com>
2024-11-08 23:41:38 +0530

    profile creation action is completed successfully

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+
+import db from "@/utils/db";
+
 import { profileSchema } from "@/utils/schema";
 
 export const createProfileAction = async (
@@ -10,7 +16,25 @@ export const createProfileAction = async (
   try {
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
+
+    // fetch current user from clerk
+    const user = await currentUser();
+
+    // checking if there is no user
+    if (!user) throw new Error("Something went wrong");
+
+    //adding profile of the user to our db
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl,
+        ...validatedFields,
+      },
+    });
+
+    // creating meta data in clerk so that when this user logs in again they will be redirected to page other than /profile/create
+
     return { message: "Profile created" };
   } catch (err) {
     console.log(err);
--- utils/schema.ts
+++ utils/schema.ts
@@ -1,7 +1,16 @@
 import * as z from "zod";
 
 export const profileSchema = z.object({
-  firstName: z.string().min(3, { message: "Aleast 3 characters needed." }),
-  lastName: z.string().min(3, { message: "Atleast 3 characters needed" }),
-  userName: z.string().min(4, { message: "Atleast 4 characters required." }),
+  firstName: z
+    .string()
+    .min(3, { message: "Aleast 3 characters needed." })
+    .trim(),
+  lastName: z
+    .string()
+    .min(3, { message: "Atleast 3 characters needed" })
+    .trim(),
+  userName: z
+    .string()
+    .min(4, { message: "Atleast 4 characters required." })
+    .trim(),
 });
