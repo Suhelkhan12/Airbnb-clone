056040f3887a4f60ea999496e349b08aa06e2200
Suhel <suhel7822@gmail.com>
2024-11-27 11:41:43 +0530

    toggle functionality for long user comment

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const Comment = ({ userComment }: { userComment: string }) => {
-  return <p className="text-sm">{userComment}</p>;
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const commentToShow =
+    !isExpanded && userComment.length > 100
+      ? userComment.slice(0, 100)
+      : userComment;
+
+  const handleClick = () => setIsExpanded((p) => !p);
+  return (
+    <div className="text-sm  ">
+      <p>
+        {commentToShow}
+        {userComment.length > 100 && (
+          <Button
+            size={"sm"}
+            variant={"link"}
+            className="p-0 mt-0 ml-1"
+            onClick={handleClick}
+          >
+            Show {isExpanded ? "less" : "more"}
+          </Button>
+        )}
+      </p>
+    </div>
+  );
 };
 
 export default Comment;
